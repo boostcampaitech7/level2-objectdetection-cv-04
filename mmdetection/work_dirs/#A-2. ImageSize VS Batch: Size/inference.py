606a2101ac177a77108036069330c6ea4e29999a
Alexaqua7 <sdh582@naver.com>
2024-10-12 15:12:18 +0900

    experience ended

---
+++ mmdetection/work_dirs/#A-2. ImageSize VS Batch: Size/inference.py
@@ -0,0 +1,124 @@
+import os
+import argparse
+import mmcv
+from mmcv import Config
+from mmdet.datasets import (build_dataloader, build_dataset,
+                            replace_ImageToTensor)
+from mmdet.models import build_detector
+from mmdet.apis import single_gpu_test
+from mmcv.runner import load_checkpoint
+from mmcv.parallel import MMDataParallel
+import pandas as pd
+from pycocotools.coco import COCO
+import numpy as np
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Faster R-CNN Inference")
+    parser.add_argument('--config', default='./configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_coco.py', help='config file path')
+    parser.add_argument('--checkpoint', default='latest', help='checkpoint to use')
+    parser.add_argument('--work-dir', default='./work_dirs/#A-2. ImageSize VS Batch: Size', help='the dir to save logs and models')
+    parser.add_argument('--gpu-id', type=int, default=0, help='id of gpu to use')
+    parser.add_argument('--root', default='../dataset/', help='root directory of dataset')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+               "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+    cfg = Config.fromfile(args.config)
+
+    # Modify dataset config
+    cfg.data.test.classes = classes
+    cfg.data.test.img_prefix = args.root
+    cfg.data.test.ann_file = os.path.join(args.root, 'test.json')
+    cfg.data.test.pipeline[1]['img_scale'] = (1024,1024)
+    cfg.data.test.test_mode = True
+
+    cfg.data.samples_per_gpu = 8
+
+    cfg.gpu_ids = [args.gpu_id]
+    cfg.work_dir = args.work_dir
+    ### 수정 부분
+    # cfg.model.bbox_head.num_classes = 10
+    for settings in cfg.model.roi_head.bbox_head:
+        settings.num_classes = 10
+    cfg.backbone = dict(
+    type='SwinTransformer',
+    embed_dims=192,  
+    depths=[2, 2, 18, 2],  
+    num_heads=[4, 8, 16, 32],  # 헤드 수 조정
+    window_size=7,
+    mlp_ratio=4.,
+    out_indices=(0, 1, 2, 3),
+    qkv_bias=True,
+    qk_scale=None,
+    drop_rate=0.0,
+    attn_drop_rate=0.0,
+    drop_path_rate=0.0, 
+    ape=False,
+    patch_norm=True,
+    out_channels=[384, 768, 1536, 3072],
+    init_cfg=dict(type='Pretrained', checkpoint='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window7_224_22k.pth')),
+    cfg.neck=dict(
+        type='PAFPN',
+        in_channels=[384, 768, 1536, 3072],
+        out_channels=256,
+        num_outs=5)
+    # cfg.runner.max_epochs = 16
+    # cfg.model.rpn_head.loss_cls=dict(
+    #         type='LabelSmoothingCrossEntropyLoss', use_sigmoid=True, loss_weight=1.0, label_smoothing = 0.12)
+    # cfg.model.rpn_head.loss_bbox=dict(type='DIoULoss')
+    ###
+    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+    cfg.model.train_cfg = None
+    
+    # build dataset & dataloader
+    dataset = build_dataset(cfg.data.test)
+    data_loader = build_dataloader(
+            dataset,
+            samples_per_gpu=1,
+            workers_per_gpu=cfg.data.workers_per_gpu,
+            dist=False,
+            shuffle=False)
+
+    # checkpoint path
+    checkpoint_path = os.path.join(cfg.work_dir, f'{args.checkpoint}.pth')
+
+    model = build_detector(cfg.model, test_cfg=cfg.get('test_cfg'))
+    checkpoint = load_checkpoint(model, checkpoint_path, map_location='cpu')
+    ### 수정
+    # cfg.model.test_cfg.rpn.nms_pre = 850
+    # cfg.model.test_cfg.rcnn.score_thr = 0.1
+    ###
+    model.CLASSES = dataset.CLASSES
+    model = MMDataParallel(model.cuda(), device_ids=[args.gpu_id])
+
+    output = single_gpu_test(model, data_loader, show_score_thr=0.05)
+
+    # Process predictions
+    prediction_strings = []
+    file_names = []
+    coco = COCO(cfg.data.test.ann_file)
+    img_ids = coco.getImgIds()
+
+    class_num = 10
+    for i, out in enumerate(output):
+        prediction_string = ''
+        image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
+        for j in range(class_num):
+            for o in out[j]:
+                prediction_string += f"{j} {o[4]} {o[0]} {o[1]} {o[2]} {o[3]} "
+        
+        prediction_strings.append(prediction_string)
+        file_names.append(image_info['file_name'])
+
+    # Create submission
+    submission = pd.DataFrame()
+    submission['PredictionString'] = prediction_strings
+    submission['image_id'] = file_names
+    submission.to_csv(os.path.join(cfg.work_dir, f'submission_b1{args.checkpoint}.csv'), index=None)
+    print(submission.head())
+
+if __name__ == '__main__':
+    main()
---
+++ mmdetection/work_dirs/#A-2. ImageSize VS Batch: Size/train.py
@@ -0,0 +1,96 @@
+import argparse
+from mmcv import Config
+from mmdet.datasets import build_dataset
+from mmdet.models import build_detector
+from mmdet.apis import train_detector
+from mmdet.datasets import (build_dataloader, build_dataset,
+                            replace_ImageToTensor)
+from mmdet.utils import get_device
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a atss_r50_fpn model")
+    # Config 관련 argument
+    parser.add_argument('--config', default='./configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_coco.py', help='config file path')
+    parser.add_argument('--work-dir', default='./work_dirs/#A-2. ImageSize VS Batch: Size', help='the dir to save logs and models')
+    parser.add_argument('--seed', type=int, default=2022, help='random seed')
+    parser.add_argument('--gpu-ids', type=int, nargs='+', default=[0], help='ids of gpus to use')
+    parser.add_argument('--samples-per-gpu', type=int, default=4, help='samples per gpu')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    # 10 classes
+    classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+               "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+    cfg = Config.fromfile(args.config)
+
+    root = '../dataset/'
+
+    # Modify dataset config
+    cfg.data.train.classes = classes
+    cfg.data.train.img_prefix = root
+    cfg.data.train.ann_file = root + 'train.json'
+    cfg.data.train.pipeline[2]['img_scale'] = (1024,1024)
+
+    cfg.data.test.classes = classes
+    cfg.data.test.img_prefix = root
+    cfg.data.test.ann_file = root + 'test.json'
+    cfg.data.test.pipeline[1]['img_scale'] = (1024,1024)
+
+    args.samples_per_gpu = 8
+    cfg.data.samples_per_gpu = args.samples_per_gpu
+    cfg.optimizer = dict(type='AdamW', lr=0.0001, weight_decay=0.0001)
+    cfg.seed = args.seed
+    cfg.gpu_ids = args.gpu_ids
+    cfg.work_dir = args.work_dir
+    ### 수정
+    for settings in cfg.model.roi_head.bbox_head:
+        settings.num_classes = 10
+    cfg.backbone = dict(
+    type='SwinTransformer',
+    embed_dims=192,  
+    depths=[2, 2, 18, 2],  
+    num_heads=[4, 8, 16, 32],  # 헤드 수 조정
+    window_size=7,
+    mlp_ratio=4.,
+    out_indices=(0, 1, 2, 3),
+    qkv_bias=True,
+    qk_scale=None,
+    drop_rate=0.0,
+    attn_drop_rate=0.0,
+    drop_path_rate=0.3, 
+    ape=False,
+    patch_norm=True,
+    out_channels=[384, 768, 1536, 3072],
+    init_cfg=dict(type='Pretrained', checkpoint='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth')),
+    cfg.neck=dict(
+        type='PAFPN',
+        in_channels=[384, 768, 1536, 3072],
+        out_channels=256,
+        num_outs=5)
+    cfg.model.rpn_head.anchor_generator.ratios = [0.25, 0.5, 1.0, 1.5, 2.0, 3.0]
+    # cfg.model.rpn_head.loss_cls=dict(
+    #         type='FocalLoss')
+    # cfg.model.rpn_head.loss_cls=dict(
+    #         type='LabelSmoothingCrossEntropyLoss', use_sigmoid=True, loss_weight=1.0, label_smoothing = 0.12)
+    # cfg.model.rpn_head.loss_bbox=dict(type='DIoULoss')
+    # cfg.lr_config.step = [12, 16]
+    # cfg.lr_config.gamma = 0.1
+    # cfg.optimizer.lr = 2e-2
+    cfg.runner.max_epochs = 20
+    ### 수정
+    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+    cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+    cfg.device = get_device()
+
+    # Build dataset
+    datasets = [build_dataset(cfg.data.train)]
+    # Build the detector
+    model = build_detector(cfg.model)
+    model.init_weights()
+
+    # Train the model
+    train_detector(model, datasets[0], cfg, distributed=False, validate=False)
+if __name__ == '__main__':
+    main()
