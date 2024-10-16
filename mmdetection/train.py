3b80c4accb93543078f2af44b9ff83342c3a1474
jongseo1111 <jongseo001111@gmail.com>
2024-10-16 13:58:08 +0900

    train 기본 설정 변경

--- mmdetection/train.py
+++ mmdetection/train.py
@@ -1,4 +1,5 @@
 import argparse
+import wandb
 from mmcv import Config
 from mmdet.datasets import build_dataset
 from mmdet.models import build_detector
@@ -7,11 +8,19 @@ from mmdet.datasets import (build_dataloader, build_dataset,
                             replace_ImageToTensor)
 from mmdet.utils import get_device
 
+
+#파일 경로 지정
+config_name = 'retinanet_x101_64x4d_fpn_1x_coco.py'
+workdir_name = 'workdir_' + config_name
+
+#Train 파일 돌 때 새로운 dir 파일 생성
+os.makedirs(workdir_name, exist_ok = True)
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Train a Faster R-CNN model")
     # Config 관련 argument
-    parser.add_argument('--config', default='./configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py', help='config file path')
-    parser.add_argument('--work-dir', default='./work_dirs/faster_rcnn_r50_fpn_1x_trash', help='the dir to save logs and models')
+    parser.add_argument('--config', default='/data/ephemeral/home/level2-objectdetection-cv-04/mmdetection/configs/retinanet/' + config_name, help='config file path')
+    parser.add_argument('--work-dir', default='/data/ephemeral/home/level2-objectdetection-cv-04/mmdetection/' + workdir_name, help='the dir to save logs and models')
     parser.add_argument('--seed', type=int, default=2022, help='random seed')
     parser.add_argument('--gpu-ids', type=int, nargs='+', default=[0], help='ids of gpus to use')
     parser.add_argument('--samples-per-gpu', type=int, default=4, help='samples per gpu')
@@ -25,18 +34,20 @@ def main():
 
     cfg = Config.fromfile(args.config)
 
-    root = '../dataset/'
+    root = 'dataset'
+
+    img_size = 512
 
     # Modify dataset config
     cfg.data.train.classes = classes
     cfg.data.train.img_prefix = root
     cfg.data.train.ann_file = root + 'train.json'
-    cfg.data.train.pipeline[2]['img_scale'] = (512,512)
+    cfg.data.train.pipeline[2]['img_scale'] = (img_size,img_size)
 
     cfg.data.test.classes = classes
     cfg.data.test.img_prefix = root
     cfg.data.test.ann_file = root + 'test.json'
-    cfg.data.test.pipeline[1]['img_scale'] = (512,512)
+    cfg.data.test.pipeline[1]['img_scale'] = (img_size,img_size)
 
     cfg.data.samples_per_gpu = args.samples_per_gpu
 
