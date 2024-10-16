e7076a50d727dc9d1ae8208527a434b5b4f393f8
jongseo1111 <jongseo001111@gmail.com>
2024-10-16 19:00:19 +0900

    나머지

---
+++ streamlit/utils.py
@@ -0,0 +1,6 @@
+def get_json_path(dataset_folder):
+    # 여기서 json 파일의 기본 경로를 설정합니다.
+    return f"../dataset/train.json"
+    # 필요에 따라 아래 주석을 해제하여 경로를 변경할 수 있습니다.
+    # return f"/data/ephemeral/home/deamin/dataset/{dataset_folder}.json"
+
